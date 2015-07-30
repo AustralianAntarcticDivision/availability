593f466c9035981e2f0e1b3a6f8ed1d7f5959c76
Ben Raymond <ben.raymond@aad.gov.au>
2015-07-31 09:32:21 +1000

    selective imports from png,geosphere; import assertthat

--- R/availability-package.R
+++ R/availability-package.R
@@ -1,9 +1,11 @@
 #' \pkg{availability}
 #'
 #' Estimating geographic space available to animals based on telemetry data
-#' 
+#'
 #' @name availability
 #' @author Ben Raymond \email{ben.raymond@@aad.gov.au}
 #' @docType package
-#' @import geosphere png
+#' @import assertthat
+#' @importFrom png readPNG
+#' @importFrom geosphere distVincentyEllipsoid finalBearing distVincentySphere
 NULL
