41963351b16935f6b8c88198a59faf99a531a5f1
Ben Raymond <ben.raymond@aad.gov.au>
2016-01-21 22:30:52 +1100

    authors updated

--- R/availability-package.R
+++ R/availability-package.R
@@ -3,7 +3,7 @@
 #' Estimating geographic space available to animals based on telemetry data
 #'
 #' @name availability
-#' @author Ben Raymond \email{ben.raymond@@aad.gov.au}
+#' @author Ben Raymond \email{ben.raymond@@aad.gov.au}, Simon Wotherspoon, Ryan Reisinger
 #' @docType package
 #' @import assertthat testthat
 #' @importFrom png readPNG
