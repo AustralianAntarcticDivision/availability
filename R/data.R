52b665ab05fc564bf731aa1971d22696b2582a14
Ben Raymond <ben.raymond@aad.gov.au>
2018-11-08 15:11:39 +1100

    include example data used in vignette

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Example PTT tracking data
+#'
+#' Three example animal tracks, from Reisinger et al. (2018)
+#'
+#' @docType data
+#'
+#' @usage data(ptt_data)
+#'
+#' @format A list containing three tracks collected using Argos PTT tags: \code{DMS} from a sooty shearwater, \code{SES} from an elephant seal, and \code{AFS} from an Antarctic fur seal.
+#'
+#' @keywords datasets
+#'
+#' @references https://doi.org/10.1111/ddi.12702
+#'
+#' @source \href{https://doi.org/10.1111/ddi.12702}{Reisinger RR, et al. (2018) Habitat modelling of tracking data from multiple marine top predators reveals important habitat in the Southern Indian Ocean. Diversity and Distributions.}
+"ptt_data"
