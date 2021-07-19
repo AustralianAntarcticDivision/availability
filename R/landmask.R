55858db7eefda30c97d3ab4bb1a569fdcd9fd834
Ben Raymond <ben.raymond@aad.gov.au>
2021-07-19 10:27:51 +1000

    minor fixes for check

--- R/landmask.R
+++ R/landmask.R
@@ -7,24 +7,24 @@
 #' @param res numeric: the spatial resolution of the mask, in degrees (either 0.1 or 0.05)
 #' @return function that returns a logical indicating whether the point is at sea (TRUE) or on land (FALSE)
 #' @seealso \code{\link{surrogateAR}}
-#' @references Wessel, P., and W. H. F. Smith, A Global Self-consistent, Hierarchical, High-resolution Shoreline Database, J. Geophys. Res., 101, #B4, pp. 8741-8743, 1996. \url{https://www.ngdc.noaa.gov/mgg/shorelines/gshhs.html}
+#' @references Wessel P, Smith WHF (1996) A Global Self-consistent, Hierarchical, High-resolution Shoreline Database. J. Geophys. Res. 101: 8741-8743. \url{https://www.ngdc.noaa.gov/mgg/shorelines/gshhs.html}
 #' @examples
 #' mask  <- gshhsMask() ## initialize land mask function
-#' mask(0,c(100,-65)) ## test point lon,lat
+#' mask(0, c(100, -65)) ## test point lon,lat
 #'
 #' @export
-gshhsMask <- function(res=0.1) {
-    if (! res %in% c(0.1,0.05)) {
-        res=0.1
+gshhsMask <- function(res = 0.1) {
+    if (!res %in% c(0.1, 0.05)) {
+        res <- 0.1
     }
-    land.mask=readPNG(system.file("extdata",paste0("land_mask_gshhs-",res,".png"),package="availability")) ## 0=land, 1=ocean
-    if (length(dim(land.mask))>1) { land.mask=land.mask[,,1] }
-    land.lon=seq(from=-180+res/2,to=180-res/2,length.out=dim(land.mask)[2])
-    land.lat=seq(from=90-res/2,to=-90+res/2,length.out=dim(land.mask)[1])
-    function(tm,pt) {
-        lonidx=which.min(abs(land.lon-((pt[[1]]+180)%%360-180)))
-        latidx=which.min(abs(land.lat-pt[[2]]))
-        land.mask[latidx,lonidx]==1
+    land.mask <- readPNG(system.file("extdata", paste0("land_mask_gshhs-", res, ".png"), package = "availability")) ## 0 = land, 1 = ocean
+    if (length(dim(land.mask)) > 1) land.mask <- land.mask[, , 1]
+    land.lon <- seq(from = -180+res/2, to = 180-res/2, length.out = dim(land.mask)[2])
+    land.lat <- seq(from = 90-res/2, to = -90+res/2, length.out = dim(land.mask)[1])
+    function(tm, pt) {
+        lonidx <- which.min(abs(land.lon-((pt[[1]]+180)%%360-180)))
+        latidx <- which.min(abs(land.lat-pt[[2]]))
+        land.mask[latidx, lonidx] > 0
     }
 }
 
@@ -33,17 +33,17 @@ gshhsMask <- function(res=0.1) {
 landmask_init <- gshhsMask
 
 ## slightly faster, but has unexpected behaviour at edges, so don't use for now
-##landmask_init=function() {
-##    land.mask=readPNG(system.file("extdata","land_mask-0.1-nosub.png",package="availability")) ## 0=land, 1=ocean
-##    land.mask=land.mask[nrow(land.mask):1,] ## reverse in latitude
-##    land.lon=seq(from=-180,to=180,length.out=dim(land.mask)[2])
-##    lon.bin=abs(land.lon[2]-land.lon[1])
-##    land.lat=seq(from=-90,to=0,length.out=dim(land.mask)[1])
-##    lat.bin=abs(land.lat[2]-land.lat[1])
-##    test_point=function(pt) {
-##        lonidx=.bincode((pt[1]+180)%%360-180,land.lon-lon.bin/2)
-##        latidx=.bincode(pt[2],land.lat-lat.bin/2)
-##        land.mask[latidx,lonidx]==1
+##landmask_init <- function() {
+##    land.mask <- readPNG(system.file("extdata", "land_mask-0.1-nosub.png", package = "availability")) ## 0 = land, 1 = ocean
+##    land.mask <- land.mask[nrow(land.mask):1, ] ## reverse in latitude
+##    land.lon <- seq(from = -180, to = 180, length.out = dim(land.mask)[2])
+##    lon.bin <- abs(land.lon[2]-land.lon[1])
+##    land.lat <- seq(from = -90, to = 0, length.out = dim(land.mask)[1])
+##    lat.bin <- abs(land.lat[2]-land.lat[1])
+##    test_point <- function(pt) {
+##        lonidx <- .bincode((pt[1]+180)%%360-180, land.lon-lon.bin/2)
+##        latidx <- .bincode(pt[2], land.lat-lat.bin/2)
+##        land.mask[latidx, lonidx] > 0
 ##    }
 ##    test_point
 ##}
@@ -56,7 +56,7 @@ landmask_init <- gshhsMask
 ##' downloaded from
 ##' \url{https://www.ngdc.noaa.gov/mgg/global/global.html}.
 ##'
-##' When the mask is intially created, a native raster (grd,gri)
+##' When the mask is intially created, a native raster (grd, gri)
 ##' version of the geotiff is created in the directory \code{tmp},
 ##' which must be writable. This file can be deleted when the
 ##' computation is finished.
@@ -73,15 +73,15 @@ landmask_init <- gshhsMask
 ##' @return a logical indicating whether the point is land or sea.
 ##' @importFrom raster raster writeRaster extract
 ##' @export
-etopoMask <- function(basename="ETOPO1_Bed_c_geotiff",path=".",tmp=path,land=FALSE) {
-  tif <- file.path(path,paste0(basename,".tif"))
-  grd <- file.path(tmp,paste0(basename,".grd"))
+etopoMask <- function(basename = "ETOPO1_Bed_c_geotiff", path = ".", tmp = path, land = FALSE) {
+  tif <- file.path(path, paste0(basename, ".tif"))
+  grd <- file.path(tmp, paste0(basename, ".grd"))
 
-  if(!file.exists(grd)) writeRaster(raster(tif),grd)
+  if(!file.exists(grd)) writeRaster(raster(tif), grd)
   etopo <- raster(grd)
 
   if(land)
-    function(tm,pt) pt[2] < 90 & pt[2] > -90 & extract(etopo,cbind((pt[1]+180)%%360-180,pt[2])) >= 0
+    function(tm, pt) pt[2] < 90 & pt[2] > -90 & extract(etopo, cbind((pt[1]+180)%%360-180, pt[2])) >= 0
   else
-    function(tm,pt) pt[2] < 90 & pt[2] > -90 & extract(etopo,cbind((pt[1]+180)%%360-180,pt[2])) <= 0
+    function(tm, pt) pt[2] < 90 & pt[2] > -90 & extract(etopo, cbind((pt[1]+180)%%360-180, pt[2])) <= 0
 }
